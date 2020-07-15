961be46637b3a0d9b18ba183c97cf675bec6e378
Aaqiel <abaaqielbehardien@gmail.com>
2020-07-15 16:41:21 +0200

    add redux to filter component

--- src/components/Products.js
+++ src/components/Products.js
@@ -95,4 +95,6 @@ class Products extends Component {
 	}
 }
 
-export default connect(state => ({ products: state.products.items }), { fetchProducts })(Products);
+export default connect(state => ({ products: state.products.filteredItems }), { fetchProducts })(
+	Products
+);
